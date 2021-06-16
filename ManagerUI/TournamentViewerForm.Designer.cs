9ecdc03db955d7b7c1811f2109df6866fc071f6a
unknown <tanmaybhargava9986@gmail.com>
2021-06-16 19:49:02 +0200

    form icon added

--- ManagerUI/TournamentViewerForm.Designer.cs
+++ ManagerUI/TournamentViewerForm.Designer.cs
@@ -29,6 +29,7 @@ namespace ManagerUI
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TournamentViewerForm));
             this.SuspendLayout();
             // 
             // TournamentViewerForm
@@ -38,6 +39,7 @@ namespace ManagerUI
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(625, 404);
             this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
             this.Name = "TournamentViewerForm";
             this.Text = "Tournament Viewer";
